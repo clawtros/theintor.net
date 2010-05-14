3464298e42c35eeb14ede5758b0c028b0432386b
Adam Benzan <adam.benzan@gmail.com>
2010-05-14 13:45:39 -0400

    add analytics

--- about.php
+++ about.php
@@ -11,6 +11,20 @@ $help = new HelpGenerator($registered_modifiers);
     <title>About The Intornet</title>
   </head>
   <body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16419330-1']);
+  _gaq.push(['_setDomainName', '.theintor.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
     <div id="about_content">
       <h1>The Intor.Net - A Helpful Guide</h1>
       <p>This is some automated documentation generated from the modifier classes for URLs.  As such, it might be a little weird.</p>
